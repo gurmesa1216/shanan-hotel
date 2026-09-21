7290feb4f8bc0b5bf1c3fa187ad31ef046248a1d
Gurme112 <gurmesafikaduu@gmail.com>
2026-09-21 07:09:36 -0700

    Increase JSON limit to 50mb for base64 images

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
+  secure: true
 });
 
 // ── 2. Configure Multer with Limits & Cloudinary Storage ──
