d1b9d3006fd0ebdab0ff753c72c2d71bc7b32069
Gurme112 <gurmesafikaduu@gmail.com>
2026-09-21 07:17:43 -0700

    Fix syntax error comma in cloudinary config

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
 });
 
